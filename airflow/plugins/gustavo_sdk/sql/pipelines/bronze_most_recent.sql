90b74039ad664e4efb6c2741c73545e65e5c2142
gustavo marcelino <gustavomarcelinodevengin@gmail.com>
2026-08-31 16:44:32 -0300

    feat: add bronze most recent pipeline

---
+++ airflow/plugins/gustavo_sdk/sql/pipelines/bronze_most_recent.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE TABLE `{project_id}.{system}_bronze.{table}` AS
+WITH base AS (
+  SELECT
+      *,
+      CURRENT_DATE() AS ingestion_date_bronze,
+      CURRENT_TIMESTAMP() AS ingestion_timestamp_bronze
+  FROM `{project_id}.{system}_raw.{table}`
+),
+dedup AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER (
+      PARTITION BY {pk}
+      ORDER BY ingestion_timestamp_raw DESC
+    ) AS rn
+  FROM base
+)
+
+SELECT
+  * EXCEPT(rn)
+FROM dedup
+WHERE rn = 1;
