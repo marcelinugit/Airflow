fb513258a9e2777489b944165e63f2714a3810c4
gustavo marcelino <gustavomarcelinodevengin@gmail.com>
2026-08-31 17:09:30 -0300

    feat: add bronze pipeline to dag

--- airflow/plugins/gustavo_sdk/app/airflow_functions.py
+++ airflow/plugins/gustavo_sdk/app/airflow_functions.py
@@ -1,119 +1,152 @@
-from gustavo_sdk.app.jobs.mysql_to_bucket import MySQLToBucketJob
-from gustavo_sdk.app.jobs.postgre_to_bucket import PostgresToBucketJob
-from gustavo_sdk.infrastructure.common.utils import get_logger
-from gustavo_sdk.infrastructure.integration.databases.mysql_client import MySQLClient
-from gustavo_sdk.infrastructure.integration.databases.postgres_client import PostgresClient
-from gustavo_sdk.app.jobs.pipelines.raw import Raw
+import logging
+from datetime import datetime
+import json
 
-logger = get_logger(__name__)
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.hooks.base import BaseHook
+
+from gustavo_sdk.app.airflow_functions import (
+    postgres_to_bucket,
+    run_raw,
+    run_bronze,
+)
 
 
-def mysql_to_bucket(
-        table_name: str,
-        bucket_name: str,
-        bucket_prefix: str,
-        file_name: str,
-        host: str,
-        database: str,
-        user: str,
-        password: str,
-        port: str,
-        credentials_json: str,
-) -> None:
-    try:
-        config = {
+logger = logging.getLogger(__name__)
+
+
+conn_postgres = BaseHook.get_connection("adventureworks_postgres")
+
+host = conn_postgres.host
+user = conn_postgres.login
+password = conn_postgres.password
+port = conn_postgres.port or 5432
+database = conn_postgres.schema
+
+
+conn_bq = BaseHook.get_connection("gcp_default")
+
+service_account_info = json.loads(
+    conn_bq.extra_dejson["keyfile_dict"]
+)
+
+project_id = service_account_info["project_id"]
+system = "adventureworks"
+schema = "adventureworks_raw"
+bucket_name = "gustavo-data-plataform-raw"
+
+today = datetime.now()
+
+ano = today.year
+mes = f"{today.month:02d}"
+dia = f"{today.day:02d}"
+
+hive_partition = (
+    f"partition_year={ano}/"
+    f"partition_month={mes}/"
+    f"partition_day={dia}"
+)
+
+
+with DAG(
+    dag_id="ingestion_postgres_adventureworks_landing",
+    start_date=datetime(2026, 8, 22),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    customer_ingestion = PythonOperator(
+        task_id="customer_ingestion",
+        python_callable=postgres_to_bucket,
+        op_kwargs={
             "host": host,
-            "database": database,
             "user": user,
             "password": password,
             "port": port,
-        }
-
-        db = MySQLClient(
-            config=config
-        )
-
-        db.connect()
-
-        job = MySQLToBucketJob(
-            db=db,
-            bucket_name=bucket_name,
-            bucket_prefix=bucket_prefix,
-            credentials_json=credentials_json,
-        )
-
-        job.run(
-            query=f"SELECT * FROM {table_name}",
-        )
-    except Exception as err:
-        logger.exception(f"MySQL ETL job failed: {err}")
-        raise
-
-
-def postgres_to_bucket(
-        table_name: str,
-        bucket_name: str,
-        bucket_prefix: str,
-        host: str,
-        database: str,
-        user: str,
-        password: str,
-        port: str,
-        credentials_json: str,
-) -> None:
-    try:
-
-        config = {
-            "host": host,
             "database": database,
-            "user": user,
-            "password": password,
-            "port": port,
-        }
-
-        db = PostgresClient(
-            config=config
-        )
-
-        db.connect()
-
-        job = PostgresToBucketJob(
-            db=db,
-            bucket_name=bucket_name,
-            bucket_prefix=bucket_prefix,
-            credentials_json=credentials_json,
-        )
-
-        job.run(
-            query=f"SELECT * FROM {table_name}",
-        )
-
-    except Exception as err:
-        logger.exception(f"PostgreSQL ETL job failed: {err}")
-        raise
-
-
-def run_raw(
-        project_id: str,
-        schema: str,
-        system: str,
-        table: str,
-        bucket_name: str,
-        year: str,
-        month: str,
-        day: str,
-        credentials_json: dict,
-) -> None:
-    raw = Raw(
-        project_id=project_id,
-        schema=schema,
-        system=system,
-        table=table,
-        bucket_name=bucket_name,
-        year=year,
-        month=month,
-        day=day,
-        credentials_json=credentials_json,
+            "table_name": "Sales.Customer",
+            "bucket_name": bucket_name,
+            "bucket_prefix": (
+                f"{system}/customer/{hive_partition}"
+            ),
+            "credentials_json": service_account_info,
+        },
     )
 
-    raw.run()
+    product_ingestion = PythonOperator(
+        task_id="product_ingestion",
+        python_callable=postgres_to_bucket,
+        op_kwargs={
+            "host": host,
+            "user": user,
+            "password": password,
+            "port": port,
+            "database": database,
+            "table_name": "Production.Product",
+            "bucket_name": bucket_name,
+            "bucket_prefix": (
+                f"{system}/product/{hive_partition}"
+            ),
+            "credentials_json": service_account_info,
+        },
+    )
+
+    customer_raw = PythonOperator(
+        task_id="customer_raw",
+        python_callable=run_raw,
+        op_kwargs={
+            "project_id": project_id,
+            "schema": schema,
+            "system": system,
+            "table": "customer",
+            "bucket_name": bucket_name,
+            "year": str(ano),
+            "month": mes,
+            "day": dia,
+            "credentials_json": service_account_info,
+        },
+    )
+
+    product_raw = PythonOperator(
+        task_id="product_raw",
+        python_callable=run_raw,
+        op_kwargs={
+            "project_id": project_id,
+            "schema": schema,
+            "system": system,
+            "table": "product",
+            "bucket_name": bucket_name,
+            "year": str(ano),
+            "month": mes,
+            "day": dia,
+            "credentials_json": service_account_info,
+        },
+    )
+
+    customer_bronze = PythonOperator(
+        task_id="customer_bronze",
+        python_callable=run_bronze,
+        op_kwargs={
+            "project_id": project_id,
+            "system": system,
+            "table": "customer",
+            "pk": "customerid",
+            "credentials_json": service_account_info,
+        },
+    )
+
+    product_bronze = PythonOperator(
+        task_id="product_bronze",
+        python_callable=run_bronze,
+        op_kwargs={
+            "project_id": project_id,
+            "system": system,
+            "table": "product",
+            "pk": "productid",
+            "credentials_json": service_account_info,
+        },
+    )
+
+    customer_ingestion >> customer_raw >> customer_bronze
+    product_ingestion >> product_raw >> product_bronze
